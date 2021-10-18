31567ce1aa8a7837426c28b9096dc5f92bb4325f
oscar <oscaroox@users.noreply.github.com>
2021-10-18 02:19:29 +0200

    convert ParserError into SpannedError

--- lib/parser/src/parser_error.rs
+++ lib/parser/src/parser_error.rs
@@ -1,12 +1,24 @@
-use scanner::{Position, Token};
+use scanner::Token;
+use spanner::SpannedError;
 use thiserror::Error;
 
 #[derive(Debug, Error, Clone, PartialEq)]
 pub enum ParserError {
     #[error("{0}")]
-    ExpectedToken(String),
-    #[error("Unexpected token: {0}")]
+    ExpectedToken(String, Token),
+    #[error("Unexpected token:")]
     UnexpectedToken(Token),
-    #[error("Expected expression after {token} at {pos} ")]
-    ExpectedExpression { token: Token, pos: Position },
+    #[error("Expected expression after")]
+    ExpectedExpression(Token),
+}
+
+impl ParserError {
+    pub fn into_spanned_error(&mut self) -> SpannedError {
+        let msg = self.to_string();
+        match self {
+            ParserError::ExpectedToken(_, tok)
+            | ParserError::ExpectedExpression(tok)
+            | ParserError::UnexpectedToken(tok) => SpannedError::new1(msg, tok.span),
+        }
+    }
 }
