33df08a1deadcdd56230cf11a5123207608808e6
oscar <oscaroox@users.noreply.github.com>
2021-10-30 15:34:35 +0200

    use clap to run cli commands

--- bin/unf/src/main.rs
+++ bin/unf/src/main.rs
@@ -1,3 +1,99 @@
+use ast::Program;
+use clap::{App, Arg, SubCommand};
+use interpreter::{Environment, Interpreter};
+use parser::{Parser, ParserError};
+use scanner::Scanner;
+use spanner::SpanManager;
+use std::{
+    cell::RefCell,
+    io::{self, BufRead, Write},
+    process,
+    rc::Rc,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Unfinished Language")
+        .version("0.1")
+        .author("Oscar D. <kbknapp@gmail.com>")
+        .about("This is the Unfinished language cli")
+        .subcommand(SubCommand::with_name("repl").about("starts the repl"))
+        .subcommand(
+            SubCommand::with_name("run")
+                .about("run a unfinished file")
+                .arg(
+                    Arg::with_name("INPUT")
+                        .help("Sets the input file to use")
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("run") {
+        let input = matches.value_of("INPUT").unwrap();
+        let source = match std::fs::read_to_string(input) {
+            Ok(contents) => contents,
+            Err(e) => panic!("{}", e),
+        };
+
+        let mut interpreter = Interpreter::new();
+        match parse_contents(source) {
+            Ok(stmts) => match interpreter.run(stmts) {
+                Ok(_) => {}
+                Err(err) => {
+                    println!("{:#?}", err);
+                    process::exit(1);
+                }
+            },
+            Err(_) => process::exit(1),
+        }
+    } else if matches.subcommand_matches("repl").is_some() {
+        let stdin = io::stdin();
+        let mut stdout = io::stdout();
+        let env = Rc::new(RefCell::new(Environment::new()));
+        println!("Welcome to the unifished language repl start typing.");
+        print!("> ");
+        stdout.flush().expect("failed to flush to stdout");
+        for line in stdin.lock().lines() {
+            let contents = line.expect("failed to read from stdin");
+            if contents == "" {
+                break;
+            }
+            let mut interpreter = Interpreter::with_env(env.clone());
+            match parse_contents(contents) {
+                Ok(stmts) => match interpreter.run(stmts) {
+                    Ok(val) => match val {
+                        interpreter::Value::Unit => {}
+                        _ => println!("{}", val),
+                    },
+                    Err(err) => {
+                        println!("{:#?}", err);
+                    }
+                },
+                Err(_) => {}
+            };
+
+            print!("> ");
+            io::stdout().flush().expect("failed to flush to stdout");
+        }
+    }
+}
+
+fn parse_contents(source: String) -> Result<Program, Vec<ParserError>> {
+    let mut manager = SpanManager::default();
+    let mut maker = manager.add_source(source.to_string());
+
+    let scanner = Scanner::new(source.to_string(), &mut maker);
+    let mut parser = Parser::new(scanner);
+
+    let (stmts, errors) = parser.parse();
+
+    if errors.len() > 0 {
+        for mut err in errors.clone() {
+            println!("{}", err.into_spanned_error().print(&manager));
+        }
+        return Err(errors);
+    }
+
+    Ok(stmts)
 }
