afdfcebac49cf0000b3c79c6a0d9552196dd6926
oscar <oscaroox@users.noreply.github.com>
2021-11-03 16:32:55 +0100

    add clone builtin

--- lib/interpreter/src/natives/builtin.rs
+++ lib/interpreter/src/natives/builtin.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 use crate::{RuntimeError, Value};
 
@@ -8,6 +8,7 @@ type BuiltinResult = Result<Value, RuntimeError>;
 pub enum Builtin {
     Println = 1,
     Len = 2,
+    Clone = 3,
 }
 
 impl Builtin {
@@ -15,6 +16,7 @@ impl Builtin {
         match &self {
             Builtin::Println => builtin_println(args),
             Builtin::Len => builtin_len(args),
+            Builtin::Clone => builtin_clone(args),
         }
     }
 
@@ -22,11 +24,12 @@ impl Builtin {
         match &self {
             Builtin::Println => String::from("last"),
             Builtin::Len => String::from("len"),
+            Builtin::Clone => String::from("clone"),
         }
     }
 
     pub fn builtins() -> Vec<Builtin> {
-        vec![Builtin::Println, Builtin::Len]
+        vec![Builtin::Println, Builtin::Len, Builtin::Clone]
     }
 }
 
@@ -35,6 +38,7 @@ impl From<u8> for Builtin {
         match f {
             0x1 => Builtin::Println,
             0x2 => Builtin::Len,
+            0x3 => Builtin::Clone,
             _ => panic!("can get builtin from {}", f),
         }
     }
@@ -53,9 +57,17 @@ pub fn get_builtins() -> HashMap<String, Value> {
         Value::native_function("len", 1, Builtin::Len),
     );
 
+    map.insert(
+        String::from("clone"),
+        Value::native_function("clone", 1, Builtin::Clone),
+    );
+
     map
 }
 
+/**
+ * Prints the value to the console
+ */
 fn builtin_println(args: Vec<Value>) -> BuiltinResult {
     match &args[0] {
         obj => {
@@ -65,6 +77,9 @@ fn builtin_println(args: Vec<Value>) -> BuiltinResult {
     }
 }
 
+/**
+ * Returns the length of an Array or String value
+ */
 fn builtin_len(args: Vec<Value>) -> BuiltinResult {
     match &args[0] {
         Value::Array(arr) => Ok(Value::Int(arr.borrow().values.len() as i64)),
@@ -72,5 +87,34 @@ fn builtin_len(args: Vec<Value>) -> BuiltinResult {
         _ => {
             panic!("invalid len")
         }
+    }
+}
+
+/**
+ * Clones a Data class isntance or Array
+ * Data classes and Arrays are references e.g
+ *
+ * let arr1 = [];
+ * let arr2 = arr1;
+ *
+ * arr2 points the arr1.
+ * using the builtin "clone" function creates a new value
+ *
+ * let arr1 = [];
+ * let arr2 = clone(arr1); // creates a new value from arr1
+ *
+ * Mutations made on arr2 wont affect arr1, mutations on arr1 will also not affect arr2
+ */
+fn builtin_clone(args: Vec<Value>) -> BuiltinResult {
+    match &args[0] {
+        Value::DataClassInstance(instance) => {
+            let i = instance.borrow();
+            Ok(Value::DataClassInstance(Rc::new(RefCell::new(i.clone()))))
+        }
+        Value::Array(arr) => {
+            let a = arr.borrow();
+            Ok(Value::Array(Rc::new(RefCell::new(a.clone()))))
+        }
+        _ => panic!("can only clone data classes"),
     }
 }
