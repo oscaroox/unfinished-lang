2e61ffde7adaf96e51678af07fa0446f6e172426
Oscar <druiventakoscar@gmail.com>
2022-12-13 20:58:57 +0100

    move into passes folder

--- lib/parser/src/lib.rs
+++ lib/parser/src/lib.rs
@@ -8,8 +8,9 @@ pub use crate::parser::*;
 use ast::Program;
 pub use passes::*;
 pub use parser_error::*;
-pub use analyzer::Analyzer;
-use passes::analyzer::AnalyzerError;
+
+use semantic_analyzer::SemanticAnalyzer;
+use passes::errors::AnalyzerError;
 
 
 pub enum Error {
@@ -19,14 +20,15 @@ pub enum Error {
 
 /**
  * Parse the input string and returns a tuple with errors and AST
- * AST might be incomplete if there are errors
+ * The errors consist of parser and semantic errors.
+ * AST may be incomplete when there are parser errors
  */
 pub fn parse(source: &str) -> (Vec<Error>, Program) {
     let mut errors = vec![];
 
     let scanner = scanner::Scanner::new(source.to_string());
     let mut parser = parser::Parser::new(scanner);
-    let mut analyzer = Analyzer::new();
+    let mut analyzer = SemanticAnalyzer::new();
 
     let (ast, parser_errors) = parser.parse();
 
@@ -45,7 +47,7 @@ pub fn parse(source: &str) -> (Vec<Error>, Program) {
 
 /**
  * Parse the input and returns a complete AST
- * Function will panic on error
+ * Function will panic if there are any parser and semantic errors
  */
 pub fn parse_panic(source: &str) -> Program {
     let (errors, ast) = parse(source);
--- lib/parser/src/passes/mod.rs
+++ lib/parser/src/passes/mod.rs
@@ -1,1 +1,2 @@
-pub mod analyzer;
+pub mod semantic_analyzer;
+pub mod errors;
